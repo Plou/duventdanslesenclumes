325f637769fb3fc78609bdd16310c275935232c4
Plou <clement@plou.io>
2018-07-04 09:32:52 +0200

    chores: Move Git root to worpdress root
      - Include elementor widgets plugin

--- index.php
+++ index.php
@@ -1,31 +1,17 @@
 <?php
 /**
- * The main template file
- * This is the most generic template file in a WordPress theme
- * and one of the two required files for a theme (the other being style.css).
- * It is used to display a page when nothing more specific matches a query.
- * E.g., it puts together the home page when no home.php file exists
+ * Front to the WordPress application. This file doesn't do anything, but loads
+ * wp-blog-header.php which does and tells WordPress to load the theme.
  *
- * Methods for TimberHelper can be found in the /lib sub-directory
- *
- * @package  WordPress
- * @subpackage  Timber
- * @since   Timber 0.1
+ * @package WordPress
  */
 
-if ( ! class_exists( 'Timber' ) ) {
-    echo 'Timber not activated. Make sure you activate the plugin in <a href="/wp-admin/plugins.php#timber">/wp-admin/plugins.php</a>';
-    return;
-}
-$context = Timber::get_context();
+/**
+ * Tells WordPress to load the WordPress theme and output it.
+ *
+ * @var bool
+ */
+define('WP_USE_THEMES', true);
 
-// We can access the loop of WordPress posts with the 'posts' variable.
-$context['posts'] = Timber::get_posts();
-
-
-// If we are on the home page, add a few other templates to our hierarchy.
-$templates = array( 'Templates/index.twig' );
-if ( is_home() ) {
-    array_unshift( $templates, 'Templates/front-page.twig', 'Templates/home.twig' );
-}
-Timber::render( $templates, $context );
+/** Loads the WordPress Environment and Template */
+require( dirname( __FILE__ ) . '/wp-blog-header.php' );
